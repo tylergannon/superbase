0e2b0c10a9a46b1232e6514199bdeaa0b4e44523
Tyler Gannon <tgannon@gmail.com>
2013-03-22 02:42:29 -0700

    Parantheses gets the correct order of operations.

--- bin/superbase.rb
+++ bin/superbase.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'simple_database'
 
 database = SimpleDatabase::Database.new
-while line = gets != nil
+while (line = gets) != nil
   line.split("\n").map(&:strip).each do |command|
     output = database.command(command)
     puts output unless output.nil?
